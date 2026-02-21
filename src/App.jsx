ae7d634e6df66380760023edb58a464cb3ea7adf
Lucas Borges da Silva <lucasborges2812@gmail.com>
2026-02-21 19:08:07 -0300

    add botão de pesquisa e função que filtra sempre que o valor do input muda

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,37 @@
-import { data } from '../data'
-import { ProductsList } from './components/products-list'
+import { useMemo, useState } from 'react';
+import { data } from '../data';
+import { ProductsList } from './components/products-list';
 
 export const App = () => {
+  const [search, setSearch] = useState("");
+
+  const SearchedProducts = useMemo(() => {
+    const filteredData = data.filter(product => {
+      return product.name.toLowerCase().includes(search.toLowerCase())
+    })
+
+    return !filteredData.length ? data : filteredData
+  }, [search]);
+
   return (
     <>
       <h1>Filtro de Itens</h1>
+      <div style={{ 
+        display: 'flex', 
+        alignItems: 'center', 
+        justifyContent: 'end', 
+        gap: '8px',
+        marginBottom: '8px'
+      }}>
+        <label htmlFor="search">Pesquisar:</label>
+        <input 
+          name='search' 
+          value={search} 
+          onChange={(e) => setSearch(e.target.value)} 
+        />
+      </div>
 
-      <ProductsList  data={data} />
+      <ProductsList data={SearchedProducts} />
     </>
   )
 }
--- src/components/products-list.jsx
+++ src/components/products-list.jsx
@@ -1,25 +1,29 @@
 
 export const ProductsList = ({ data }) => {
-  const td = {
+  const border = {
     border: '1px solid #DDD', 
     textAlign: "left", 
     padding: '8px',
   }
   
   return (
-    <table style={{width: '100%', borderCollapse: "collapse"}} >
-      <tr>
-        <th style={td}>Id</th> 
-        <th style={td}>Name</th>
-      </tr>
-      {data.map(item => {
-        return (
-          <tr key={item.id}>
-            <td style={td}>{item.id}</td>
-            <td style={td}>{item.name}</td>
-          </tr>
-        )
-      })}
+    <table style={{width: '100%', borderCollapse: "collapse"}}>
+      <thead>
+        <tr>
+          <th style={border}>Id</th> 
+          <th style={border}>Name</th>
+        </tr>
+      </thead>
+      <tbody>
+        {data.map(item => {
+          return (
+            <tr key={item.id}>
+              <td style={border}>{item.id}</td>
+              <td style={border}>{item.name}</td>
+            </tr>
+          )
+        })}
+      </tbody>
     </table>
   )
 };
